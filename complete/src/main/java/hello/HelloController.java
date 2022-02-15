4ac94d0b25e8344261e16ed40f42e2314d0c4936
Rengarajang <Rengarajang@yahoo.com>
2022-02-15 19:57:53 +0530

    Update HelloController.java

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from CI/CD demo !";
+        return "Greetings from CICD demo !";
     }
     
 }
